4a66f4ba8f6871a07380bdbe4a6b367649403e2e
RemiSaurel <r.saurel5@gmail.com>
2024-06-29 23:52:26 +0200

    fix: multi-select

--- edutheque-front/components/research/Container.vue
+++ edutheque-front/components/research/Container.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { levels, subjects } from "~/models";
 
-const levelSelected = ref<string>("");
-const subjectSelected = ref<string>("");
+const levelListSelected = ref<string[]>([]);
+const subjectListSelected = ref<string[]>([]);
 </script>
 
 <template>
@@ -14,19 +14,35 @@ const subjectSelected = ref<string>("");
     </UFormGroup>
     <ResearchVerticalDivider class="hidden md:flex" />
     <UFormGroup label="Niveaux">
-      <UInputMenu
-        v-model="levelSelected"
+      <USelectMenu
+        v-model="levelListSelected"
         :options="levels"
-        placeholder="Quel(s) niveau(x) ?"
-      />
+        multiple
+        searchable
+        searchable-placeholder="Quel(s) niveau(x) ?"
+        placeholder="Rechercher par niveau(x)"
+        class="w-48"
+      >
+        <template #label class="text-ellipsis truncate">
+          <span v-if="levelListSelected.length" class="text-ellipsis truncate">{{ levelListSelected.join(', ') }}</span>
+        </template>
+      </USelectMenu>
     </UFormGroup>
     <ResearchVerticalDivider class="hidden md:flex" />
     <UFormGroup label="Matières">
-      <UInputMenu
-        v-model="subjectSelected"
+      <USelectMenu
+        v-model="subjectListSelected"
         :options="subjects"
-        placeholder="Quelle(s) matière(s) ?"
-      />
+        multiple
+        searchable
+        searchable-placeholder="Quelle(s) matière(s) ?"
+        placeholder="Rechercher par matière(s)"
+        class="w-48"
+      >
+        <template #label>
+          <span v-if="subjectListSelected.length" class="text-ellipsis truncate">{{ subjectListSelected.join(', ') }}</span>
+        </template>
+      </USelectMenu>
     </UFormGroup>
     <ResearchVerticalDivider class="hidden md:flex" />
     <UFormGroup label="Auteur">
