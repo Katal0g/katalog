8dcc61744b2e6c4343d8f8da1f4c2a649438328b
RemiSaurel <r.saurel5@gmail.com>
2024-06-29 21:10:54 +0200

    chore: add uno + basic setup

---
+++ edutheque-front/components/navbar/Navbar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
