888420f2622285f8578bd4d4836de82185ce4021
RemiSaurel <r.saurel5@gmail.com>
2024-06-30 10:47:44 +0200

    feat: combined tags

--- edutheque-front/components/ressource/Tag.vue
+++ edutheque-front/components/ressource/Tag.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
+import type { Tag } from "~/models";
+import type {PropType} from "vue";
 defineProps({
   label: {
-    type: String,
+    type: String as PropType<Tag>,
   },
 });
 </script>
 
 <template>
-  <div
-    class="flex h-fit w-fit justify-center rounded-2xl bg-slate-900 px-2 text-center text-sm text-white py-0.5"
-  >
-    {{ label }}
+  <div>
+    <div
+        class="flex h-fit w-fit justify-center rounded-2xl bg-slate-900 px-2 text-center text-sm text-white py-0.5"
+        v-if="label"
+    >
+      {{ label }}
+    </div>
   </div>
+
 </template>
 
 <style scoped></style>
--- edutheque-front/utils/faker.ts
+++ edutheque-front/utils/faker.ts
@@ -1,9 +1,10 @@
 import type { Ressource } from "~/models";
 import { faker } from "@faker-js/faker/locale/fr";
-import { subjects } from "~/models";
+import {levels, subjects} from "~/models";
 
 export const generateResources = (length: number): Ressource[] => {
   const resources: Ressource[] = [];
+  const combinedTags = [...subjects, ...levels];
 
   for (let i = 0; i < length; i++) {
     // Just to have uppercase first letter
@@ -13,9 +14,10 @@ export const generateResources = (length: number): Ressource[] => {
     const description = faker.lorem.sentences(3);
     const link = faker.internet.url();
     const nbTags = Math.floor(Math.random() * 4) + 1;
+    // Take from subjects arrays or levels array or both
     const tags = Array.from(
-      { length: nbTags },
-      () => subjects[Math.floor(Math.random() * subjects.length)],
+        { length: nbTags },
+        () => combinedTags[Math.floor(Math.random() * combinedTags.length)]
     );
 
     resources.push({ title, image, description, link, tags });
