cf590597bca1de651cbf9fa045e00d222cf2e585
RemiSaurel <r.saurel5@gmail.com>
2024-07-14 16:50:50 +0200

    chore: setup i18n

---
+++ edutheque-front/i18n.config.ts
@@ -0,0 +1,68 @@
+export default defineI18nConfig(() => ({
+  legacy: false,
+  locale: "fr",
+  messages: {
+    en: {
+      generator: "Generator",
+      ressources: "Resources",
+      help: "Help",
+      generatorPage: {
+        content: "Content",
+        export: "Export",
+        contentPlaceholder: "Your content will be generated here...",
+        generateContent: "Generate the content",
+        nbQuestions: "Nb of questions",
+        robotsWorking:
+          "Our little robots are working hard to generate the content...",
+      },
+      researchPage: {
+        author: "Author",
+        moreInfos: "More informations",
+        search: "Search",
+        use: "Use",
+      },
+      scholar: {
+        level: "Level",
+        subject: "Subject",
+        title: "Title",
+      },
+      utils: {
+        select: "Select a {field}",
+        search: "Search by {field}",
+        enter: "Enter your {field}",
+        required: "Required",
+      },
+    },
+    fr: {
+      generator: "Générateur",
+      ressources: "Ressources",
+      help: "Aide",
+      generatorPage: {
+        content: "Contenu",
+        export: "Exporter",
+        contentPlaceholder: "Votre contenu sera généré ici...",
+        generateContent: "Générer le contenu",
+        nbQuestions: "Nb de questions",
+        robotsWorking:
+          "Nos petits robots travaillent dur pour générer le contenu...",
+      },
+      researchPage: {
+        author: "Auteur",
+        moreInfos: "Plus d'informations",
+        search: "Rechercher",
+        use: "Utiliser",
+      },
+      scholar: {
+        level: "Niveau",
+        subject: "Matière",
+        title: "Titre",
+      },
+      utils: {
+        select: "Sélectionner un {field}",
+        search: "Rechercher par {field}",
+        enter: "Entrez votre {field}",
+        required: "Obligatoire",
+      },
+    },
+  },
+}));
--- edutheque-front/nuxt.config.ts
+++ edutheque-front/nuxt.config.ts
@@ -5,7 +5,18 @@ export default defineNuxtConfig({
   },
   css: ["~/assets/css/main.css"],
   devtools: { enabled: true },
-  modules: ["@unocss/nuxt", "@nuxt/ui"],
+  modules: [
+    "@unocss/nuxt",
+    "@nuxt/ui",
+    [
+      "@nuxtjs/i18n",
+      {
+        i18n: {
+          vueI18n: "./i18n.config.ts",
+        },
+      },
+    ],
+  ],
   runtimeConfig: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   },
