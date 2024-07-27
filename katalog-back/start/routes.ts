4988ec127fb215e349130e524b926a305e204ddb
Yohan Lussiez <yohan.lussiez@outlook.fr>
2024-07-27 22:10:13 +0200

    chore: removed useless routes

--- katalog-back/start/routes.ts
+++ katalog-back/start/routes.ts
@@ -4,9 +4,6 @@ const resourcesController = () => import('#controllers/resources_controller')
 router
   .group(() => {
     router.get('/resources', [resourcesController, 'index'])
-    router.post('/resources', [resourcesController, 'store'])
     router.get('/resources/:id', [resourcesController, 'show'])
-    router.put('/resources/:id', [resourcesController, 'update'])
-    router.delete('/resources/:id', [resourcesController, 'destroy'])
   })
   .prefix('api/v1')
