83c50c5fb63470bc7415ca514adc54c20abb5489
Yohan Lussiez <yohan.lussiez@outlook.fr>
2024-07-27 22:01:37 +0200

    chore: created services for all type of resources (Forge, Elaastic)

--- katalog-back/app/controllers/resources_controller.ts
+++ katalog-back/app/controllers/resources_controller.ts
@@ -1,27 +1,25 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import ElaasticResource from '#models/elaastic_resource'
 
-export default class ElaasticResourcesController {
-  // Méthode pour lister toutes les ressources
+// TODO : Make resources controller gather all types of resources (Forge, Elaastic)
+
+export default class ResourcesController {
   async index({ response }: HttpContext) {
     const resources = await ElaasticResource.all()
     return response.json(resources)
   }
 
-  // Méthode pour créer une nouvelle ressource
   async store({ request, response }: HttpContext) {
     const data = request.only(['title', 'description', 'author', 'link', 'tags'])
     const resource = await ElaasticResource.create(data)
     return response.status(201).json(resource)
   }
 
-  // Méthode pour afficher une ressource spécifique
   async show({ params, response }: HttpContext) {
     const resource = await ElaasticResource.findOrFail(params.id)
     return response.json(resource)
   }
 
-  // Méthode pour mettre à jour une ressource spécifique
   async update({ params, request, response }: HttpContext) {
     const resource = await ElaasticResource.findOrFail(params.id)
     const data = request.only(['title', 'description', 'lastUpdated', 'author', 'link', 'tags'])
@@ -30,10 +28,9 @@ export default class ElaasticResourcesController {
     return response.json(resource)
   }
 
-  // Méthode pour supprimer une ressource spécifique
   async destroy({ params, response }: HttpContext) {
     const resource = await ElaasticResource.findOrFail(params.id)
     await resource.delete()
-    return response.status(204).send()
+    return response.status(204).send(null)
   }
 }
---
+++ katalog-back/app/services/elaastic_resources_service.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosInstance } from 'axios'
+
+class ElaasticResourcesService {
+  private axiosInstance: AxiosInstance
+
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: 'http://localhost:3333/api/v1', // Changez l'URL de base si nécessaire
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  }
+
+  async getAllResources() {
+    try {
+      const response = await this.axiosInstance.get('/elaasticResources')
+      return response.data
+    } catch (error) {
+      throw new Error(error.response.data.message || 'Failed to fetch resources')
+    }
+  }
+}
+
+export default new ElaasticResourcesService()
