3510449717f9867d9caf22411a2efef96e77dff1
Yohan Lussiez <yohan.lussiez@outlook.fr>
2024-07-27 21:18:47 +0200

    chore : Succesful migrations : created Elaastic resources migrations

---
+++ katalog-back/database/migrations/1722107542472_create_elaastic_resources_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'elaastic_resources'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title', 255).notNullable()
+      table.text('description')
+      table.string('author', 255)
+      table.text('link').notNullable()
+      table.text('tags') // Assuming tags are stored as a string
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
